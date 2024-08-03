910bdccadcf1ef2babbeb064aa5f377b2a19fab1
Gabriel-Alves-13 <gabrielso.alves@gmail.com>
2024-08-02 21:29:57 -0300

    primeiraParteAula

---
+++ unidade1/gabarito_aula4/Aluno.java
@@ -0,0 +1,66 @@
+
+public class Aluno {
+
+    String nome = null;
+    double nota1 = 5;
+    double nota2 = 0;
+    double nota3 = 0;
+    String nomeDaMae = "Maezinha";
+
+    public String getNomeMaisculo() {
+        return nome.toUpperCase();
+    }
+
+    public String getNomeMinusculo() {
+        return nome.toLowerCase();
+    }
+
+    public double getMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    public boolean aprovadoMedia() {
+        if (getMedia() >= 7)
+            return true;
+        return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno [nome=" + nome + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nomeDaMae="
+                + nomeDaMae + "]";
+    }
+
+}
---
+++ unidade1/gabarito_aula4/TestarListaAlunos.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class TestarListaAlunos {
+
+    public static void main(String[] args) {
+        
+    
+        LinkedList<Aluno>osAlunos=new LinkedList<Aluno>();
+        for (int i = 0; i < 89764; i++) {
+            Aluno mae = new Aluno();
+            mae.setNome ("Mae"+ i);
+            mae.setNota1(5);
+                
+        System.out.println(mae);
+        }
+    }
+}
