5f79f4192f4e00f4e2ab9a3ff36c2e1980a20204
Gabriel-Alves-13 <gabrielso.alves@gmail.com>
2024-08-15 23:44:29 -0300

    Tentando organizar pastas e questao1

--- unidade1/exercicio1/br/com/ap3/gabriel/Questao1/Questao1Editada.java
+++ unidade1/exercicio1/br/com/ap3/gabriel/Questao1/Questao1Editada.java
@@ -1,12 +1,16 @@
-package br.com.ap3.distancia;
+package br.com.ap3.gabriel.Questao1;
 
 import java.util.Scanner;
 
+public static void main(String[] args) {
+    sysout(getDistancia());
+    sysout(getAmplitude());
+  }
 public class Questao1 {
   public double getDistancia;
   {
 
-  Scanner teclado = new Scanner(System.out);
+  Scanner teclado = new Scanner(System.in);
 
   double x1, y1, x2, y2, distancia = 1.0;
 
@@ -19,24 +23,18 @@ public class Questao1 {
   distancia=Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),1/2);
 
   System.out.println("A distância é: "+distancia);
-    return 0.1;
+    return;
   }
 
   }
 
   public double getAmplitude();{
   
-  double val1 = 0.0,val2=a,val3=-a;
+  double val1 = 0.0, val2=0.0, val3=-0.1;
 
   double amplitude2() {
     return val1 - val2;
-  }return
+  }
 
   amplitude2()-val3;  
-    }return-1;
-
-  public static void main(String[] args) {
-      sysout(getDistancia());
-      sysout(getAmplitude());
     }
-}
