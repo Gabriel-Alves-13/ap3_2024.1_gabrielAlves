8d911a6ba4655bae59c6082433c77d313c16432e
Gabriel-Alves-13 <gabrielso.alves@gmail.com>
2024-09-13 19:42:55 -0300

    mostrando para meu pai

--- unidade1/exercicos2/Package_recursos_humanos/MainRecursosHumanos.java
+++ unidade1/exercicos2/Package_recursos_humanos/MainRecursosHumanos.java
@@ -1,6 +1,7 @@
 package unidade1.exercicos2.Package_recursos_humanos;
 /*
- * Adicionar produto não funciona, pois a adicão do produto é uma classe privada (e está em outa pasta)
+ * Adicionar produto não funciona, pois a adicão do produto é uma 
+ * classe privada (e está em outa pasta)
  */
 public class MainRecursosHumanos {
 
